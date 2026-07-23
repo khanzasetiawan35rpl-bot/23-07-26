03667ddafd565c06f2b7d03019299a0f752f7021
Khanza Jasmine Setiawan X_RPL_6 <khanza_setiawan_35rpl@student.smktelkom-mlg.sch.id>
2026-07-23 08:31:31 +0700

    Add files via upload

---
+++ Khanza.js
@@ -0,0 +1,4 @@
+console.log("Nama : Khanza Jasmine Setiawan");
+console.log("Tanggal Lahir : 11 April 2010");
+console.log("SMP :MTs Negari Kota Probolinggo");
+console.log("Saya masuk SMK Telkom Karena saya ingin memperdalam belajar tentang coding");
